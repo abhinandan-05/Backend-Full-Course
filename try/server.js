ae7d225625ecb58a3a347276eb71b41f97e8f5c4
Abhinandan Kumar <abhicpr05@gmail.com>
2025-04-20 12:46:05 +0530

    first backend app

--- try/server.js
+++ try/server.js
@@ -1,3 +1,7 @@
+// The address of this server connected to network is : 
+// URL -> http://localhost:8383
+// IP -> 127.0.0.1:8383
+
 const express = require("express");
 const app = express();
 const PORT = 8383;
